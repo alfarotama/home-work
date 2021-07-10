c629d832872a30b6538773d5cabcf7ace41033f4
alfarotama <alfarohadiyantama13@gmail.com>
2021-07-10 07:53:03 +0700

    problem axios still doesn't work properlly

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,23 @@
 function formSubmit() {
 	alert("Have A Great Day");
 }
+
+// adding axios for API track
+axios
+	.get(
+		"https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
+	)
+	.then(function (response) {
+		//handle success
+		console.log(response.data);
+
+		const trackTitleDOM = document.getElementById("track-title");
+		trackTitleDOM.innerHTML = `Track title: ${response.data.name}`;
+
+		const artistDOM = document.getElementById("artist");
+		artistDOM.innerHTML = `Artist: ${response.data.artist[0]}`;
+	})
+	.catch(function (error) {
+		//handle error
+		console.log(error);
+	});
